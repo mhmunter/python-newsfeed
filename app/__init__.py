180a2430489a531f9dc40826cc85347959f06263
mhmunter <mark.munter1@gmail.com>
2022-10-28 11:59:26 -0500

    added verrification

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
-from app.routes import home, dashboard
+from app.routes import home, dashboard, api
 from flask import Flask
-
+from app.db import init_db
+from app.utils import filters
 
 
 
@@ -13,14 +14,19 @@ def create_app(test_config=None):
     SECRET_KEY='super_secret_key'
   )
 
-  @app.route('/hello')
-  def hello():
-    return 'hello world'
+  
+
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+  app.register_blueprint(api)
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
+  init_db(app)
 
-  
   return app
 
 
-  
+  #@app.route('/hello')
+  #def hello():
+   # return 'hello world'
